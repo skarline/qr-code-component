dc1677a1bb1af766557e7fca336b86a823ca025b
skarline <lihue-molina@hotmail.com>
2022-09-05 14:22:18 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import MyCard from "./components/MyCard.vue"
+</script>
+
+<template>
+  <MyCard image="/image-qr-code.png" alt="frontendmentor.io">
+    <h1>Improve your front-end skills by building projects</h1>
+    <p>
+      Scan the QR code to visit Frontend Mentor and take your coding skills to
+      the next level
+    </p>
+  </MyCard>
+</template>
+
+<style>
+body {
+  font-family: "Outfit", sans-serif;
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  background-color: hsl(212, 45%, 89%);
+}
+h1 {
+  color: hsl(218, 44%, 22%);
+  font-size: 22px;
+}
+p {
+  color: hsl(220, 15%, 55%);
+  font-size: 15px;
+}
+</style>
---
+++ src/components/MyCard.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const props = defineProps({
+  image: {
+    type: String,
+    required: true
+  },
+  alt: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="card">
+    <img :src="props.image" :alt="props.alt" />
+    <div class="card-content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 20rem;
+  padding: 1rem;
+  box-sizing: border-box;
+  border-radius: 1rem;
+  background-color: hsl(0, 0%, 100%);
+  box-shadow: 0 1.5rem 1.5rem hsl(220, 15%, 55%, 10%);
+}
+.card-content {
+  text-align: center;
+  padding: 0.25rem 1rem 0.5rem 1rem;
+}
+img {
+  width: 100%;
+  border-radius: 0.5rem;
+}
+</style>
